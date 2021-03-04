bd5a710b2d4cd778f68ae0df810e362bc403888a
Priscila <priscilassilveira97@gmail.com>
2021-03-04 20:35:06 -0300

    criando post de pedidos na api

--- src/pages/hall/Hall.jsx
+++ src/pages/hall/Hall.jsx
@@ -2,15 +2,23 @@
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Menu from "../../containers/Menu";
-import Cart from "../../containers/Cart";
 import Logo from '../../images/logoLaranja.png';
 import { USER } from "../../components/api";
-import { MenuOrders, DivMenus, LogoHall, ButtonPedidos } from '../../components/stylesMenu';
+
+// import { FaTrashAlt } from 'react-icons/fa';
+import { MenuOrders, DivMenus, LogoHall, ButtonPedidos, Textarea, SpamQtd, DivTotal, Soma, Total, Itens } from '../../components/stylesMenu';
 
 const Hall = () => {
     const [menuData, setMenuData] = useState({});
     const [cartData, setCartData] = useState({});
     const [cartTotal, setCartTotal] = useState(0);
+    const [order, setOrder] = useState([]);
+    const [table, setTable] = useState('')
+    const [client, setClient] = useState('');
+    const [response, setResponse] = useState('');
+    const [observation, setObservation] = useState('')
+    const [status, setStatus] = useState('pending')
+
 
     useEffect(async function (token) {
         const { url, options } = USER(token);
@@ -40,7 +48,6 @@ const Hall = () => {
         }
         setCartData(newCart);
     };
-
     const reduceFromCart = sku => {
         let newCart = { ...cartData };
         if (sku in cartData) {
@@ -49,9 +56,46 @@ const Hall = () => {
         }
         setCartData(newCart);
     };
+    function createOrder(client, table) {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", `${token}`);
+        myHeaders.append("Content-Type", "application/json");
 
+        const products = (Object.keys(cartData).map((sku) => ([`
+            "qtd": ${cartData[sku]}  
+            "id":${menuData[sku].id} 
+            ${menuData[sku].name}
+         `,])))
+
+        const raw = JSON.stringify({
+            "status": status,
+            "client": client,
+            "table": table,
+            "products": products
+         });
+        console.log(raw);
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("https://lab-api-bq.herokuapp.com/orders", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setOrder(result.json)
+                setResponse(result.json)
+                console.log(result);
+                setStatus(result.status)
+                console.log(result.status)
+            })
+    }
+    const token = localStorage.getItem('token');
     return (
         <div>
+
             <Link to='/orders'>
                 <ButtonPedidos>Pedidos</ButtonPedidos>
             </Link>
@@ -60,19 +104,108 @@ const Hall = () => {
                 <div>
                     <Menu
                         menu={menuData}
-                        addToCart={addToCart} 
-                        />
+                        addToCart={addToCart}
+                    />
                 </div>
                 <MenuOrders>
-                    <Cart
-                        total={cartTotal}
-                        menu={menuData}
-                        cart={cartData}
-                        reduceCart={reduceFromCart} />
+
+                    <div id="cart">
+                        <label htmlFor="client">Cliente</label>
+                        <input
+                            type="text"
+                            id="client"
+                            value={client}
+                            onChange={(event) => {
+                                setClient(event.target.value);
+                                localStorage.setItem('client', client);
+                            }}
+                        />
+                        <label htmlFor="table">Mesa</label>
+                        <input
+                            type="text"
+                            id="table"
+                            value={table}
+                            onChange={(event) => {
+                                setTable(event.target.value);
+                                localStorage.setItem('table', table);
+                            }}
+                        />
+                        <SpamQtd>
+                            <Itens>Item</Itens>
+                            <Itens>Qtd.</Itens>
+                            <Itens>Valor</Itens>
+                        </SpamQtd>
+
+                        <div id="cart-area">
+                            {
+                                Object.keys(cartData).map((sku, index) => (
+
+                                    <div className="cart-item" key={index}>
+
+
+                                        <div className="item-text">
+                                            <p>
+                                                {menuData[sku].name}
+                                            </p>
+                                            <p>{cartData[sku]}</p>
+                                            <span className="item-total">
+                                                R${menuData[sku].price},00
+                                    <button
+                                                    className="remove-button"
+                                                    onClick={() => reduceFromCart(sku)}
+                                                    style={{
+                                                        'border': 'none',
+                                                        'outline': 'none',
+                                                        'backgroundColor': 'transparent',
+                                                        'padding': '5px',
+                                                        'color': '#E65100',
+                                                        'cursor': 'pointer'
+                                                    }}>
+                                                    - </button>
+                                            </span>
+                                        </div>
+                                        <span>
+                                            <p>
+                                                {menuData[sku].flavor}
+                                            </p>
+                                            <div>
+                                                {menuData[sku].complement}
+                                            </div>
+
+                                        </span>
+                                    </div>
+                                ))
+                            }
+
+                            <div id="cart-total">
+                                <Textarea
+                                    name="Observations"
+                                    cols="47" rows="2"
+                                    placeholder="Observações"
+                                    value={observation}
+                                    onChange={(event) => {
+                                        setObservation(event.target.value);
+                                        localStorage.setItem('observation', observation);
+                                    }}
+                                >
+                                </Textarea>
+                                <DivTotal>
+                                    <Total>Total </Total>
+                                    <Soma>
+                                        R$ <span id="total-amount">{cartTotal},00</span>
+                                    </Soma>
+                                </DivTotal>
+                                {response && response.ok && <p>Seu pedido foi enviado</p>}
+                                <button onClick={() => createOrder(client, table, order)}>Enviar Pedido</button>
+                            </div>
+                        </div>
+                    </div>
+
                 </MenuOrders>
             </DivMenus>
         </div>
     );
+
 }
 
 export default Hall;
